67b64fa26ab4bb36dd8681fd710962ff2a3a514d
gasolin <gasolin@gmail.com>
2016-12-25 16:22:02 +0800

    update to based on yeoman 1.0, fix #1

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,43 +1,54 @@
 'use strict';
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
+const path = require('path');
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
 
-module.exports = yeoman.Base.extend({
-  prompting: function () {
+module.exports = class extends Generator {
+  initializing() {
+    this.props = {};
+  }
+
+  prompting() {
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the stylish ' + chalk.red('generator-neutrino-react') + ' generator!'
+      'Welcome to ' + chalk.red('neutrino-react') + ' generator!'
     ));
 
-    // var prompts = [{
-    //   type: 'confirm',
-    //   name: 'someAnswer',
-    //   message: 'Would you like to enable this option?',
-    //   default: true
-    // }];
+    var prompts = [{
+      type: 'input',
+      name: 'projectName',
+      message: 'Your project name',
+      default: path.basename(process.cwd())
+    }];
 
-    // return this.prompt(prompts).then(function (props) {
-    //   // To access props later use this.props.someAnswer;
-    //   this.props = props;
-    // }.bind(this));
-  },
+    return this.prompt(prompts).then(props => {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+    });
+  }
 
-  writing: function () {
-    this.directory('src', 'src');
+  writing() {
+    this.fs.copy(
+      this.templatePath('src/**/*'),
+      this.destinationPath('src/')
+    );
 
     this.fs.copy(
       this.templatePath('README.md'),
       this.destinationPath('README.md')
     );
 
-    this.fs.copy(
-      this.templatePath('package.json'),
-      this.destinationPath('package.json')
+    this.fs.copyTpl(
+      this.templatePath('_package.json'),
+      this.destinationPath('package.json'),
+      {
+        projectName: this.props.projectName
+      }
     );
-  },
+  }
 
-  install: function () {
+  install() {
     this.installDependencies();
   }
-});
+};
