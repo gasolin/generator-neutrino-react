bddb8772b2b55e7081babd936c9863461e59ce7b
gasolin <gasolin@gmail.com>
2016-12-25 19:25:03 +0800

    add hint for template

--- generators/app/templates/src/components/app.js
+++ generators/app/templates/src/components/app.js
@@ -4,7 +4,8 @@ export default function App() {
   return (
     <div>
       <h1>Hello Neutrino!</h1>
-      This content is located in <mark>src/components/app.js</mark>.
+      HTML files are built using the <mark>src/template.ejs</mark>;
+      This content is located in <mark>src/components/app.js</mark> as a react component.
     </div>
   );
 }
